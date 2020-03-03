1a33fafbc10ae3d5340620232038d8da34a3df73
dieguiviti <dieguiviti@gmail.com>
2020-03-03 23:37:07 +0100

    database config and routes defined and applied to server

---
+++ config/db.js
@@ -0,0 +1,26 @@
+const MONGOOSE = require('mongoose');
+const CONFIG = require('config');
+
+// URI
+const DB_URI = CONFIG.get('mongoURI');
+const DB_OPTIONS = {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true
+};
+
+// Connect to URI using an asynchronous function
+const CONNECT_DB = async () => {
+  try {
+    await MONGOOSE.connect(DB_URI, DB_OPTIONS);
+
+    console.log('MongoDB CONNECTED WITH SUCCESS');
+  } catch (error) {
+    console.log(error.message);
+    // Exit process with failure
+    process.exit(1);
+  }
+};
+
+// Export Connection configuration
+module.exports = CONNECT_DB;
---
+++ server.js
@@ -0,0 +1,34 @@
+const EXPRESS = require('express');
+const CONNECT_DB = require('./config/db');
+
+/* Key configuration with dotenv (.env) as an alternative to config
+    const DOT_ENV = require('dotenv');
+    DOT_ENV.config();
+*/
+
+// Initialize Express application
+const APP = EXPRESS();
+
+// Connect to database
+CONNECT_DB();
+
+/* Test request
+    APP.get('/', (request, response) => response.send('API RUNNING'));
+*/
+
+// Define ROUTERS
+const USERS_ROUTER = require('./routes/api/users');
+const PROFILES_ROUTER = require('./routes/api/profiles');
+const POSTS_ROUTER = require('./routes/api/posts');
+const AUTH_ROUTER = require('./routes/api/auth');
+// Define Routes
+APP.use('/api/users', USERS_ROUTER);
+APP.use('/api/profiles', PROFILES_ROUTER);
+APP.use('/api/posts', POSTS_ROUTER);
+APP.use('/api/auth', AUTH_ROUTER);
+
+//Determine post
+const PORT = process.env.PORT || 5000;
+
+// Application listen
+APP.listen(PORT, () => console.log(`SERVER IS LISTENING ON PORT: [${PORT}]`));
